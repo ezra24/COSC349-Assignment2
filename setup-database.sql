6d4813fbe7c8ca42ec1992e6f12effc22e180b4a
ezra24 <87739977+ezra24@users.noreply.github.com>
2021-10-18 00:10:11 +1400

    Add files via upload

---
+++ setup-database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE person (
+  code varchar(7),
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (code)
+);
+
+INSERT INTO person VALUES ('Euta Ezra Collins');
+INSERT INTO person VALUES ('LeBron James');
+INSERT INTO person VALUES ('Michael James');
+
